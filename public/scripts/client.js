c838568faa2055743d7cb2c4663f826306706b03
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-11-10 10:55:54 -0600

    addl styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   console.log('up and running!');
 
+//calling two get requests to post both our bios and our like count to the DOM
 getCoolCats();
 getCoolLikes();
 
+//setting empty cool object so that our post request is formatted correctly
 var cool = {
   name: ''
 };
 
+//function to get our bios from the server
 function getCoolCats() {
   $.ajax({
     type: 'GET',
@@ -19,15 +22,17 @@ function getCoolCats() {
   });
 }
 
+//function to append our Bios to the DOM
 function appendCoolCats(coolCats) {
   $('#showcase').empty();
 
   for (var i = 0; i < coolCats.length; i++) {
-    $('#showcase').append('<div><h2>' + coolCats[i].name + '</h2><p>' + coolCats[i].bio + '</p>' + '<img src="' + coolCats[i].image + '"><button class="like-button" id="' + coolCats[i].name + '">Like ' + coolCats[i].name +'</button></div>');
+    $('#showcase').append('<div id="catContainer"><h2>' + coolCats[i].name + '</h2><p>' + coolCats[i].bio + '</p>' + '<img src="' + coolCats[i].image + '"><button class="like-button" id="' + coolCats[i].name + '">Like ' + coolCats[i].name +'</button></div>');
 
   }
 }
 
+//click event for liking cool cats
 $('#showcase').on('click', '.like-button', function (){
 
   var name = $(this).attr("id");
@@ -35,6 +40,8 @@ $('#showcase').on('click', '.like-button', function (){
   likeCoolCat(cool);
 });
 
+//function to post a like for a cool cat to the server, empty the likeDiv, then
+//reappend the like data to the DOM with another get request
 function likeCoolCat (name) {
   event.preventDefault();
     console.log(name);
@@ -52,6 +59,7 @@ function likeCoolCat (name) {
 
 
 
+//function to get like data from the server, then append that info to the DOM
 function getCoolLikes() {
   $.ajax({
     type: 'GET',
@@ -63,6 +71,7 @@ function getCoolLikes() {
   });
 }
 
+//function to clear out likeDiv, then append like data to the DOM
 function appendCoolLikes(coolLikes) {
   $('#likeDiv').empty();
   $('#likeDiv').append('<p>Likes 4 Emily: '+ coolLikes["Emily"] +'</p>');
